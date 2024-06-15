3f91f29d5fe24be89a27adda9ad817ffdc1c93c5
qkrxodud00 <qkrxodud00@gmail.com>
2024-06-15 11:58:28 +0900

    *feat : 프로젝트 초기값

---
+++ src/main/java/com/jobisnvillains/UserRefundCalculator/UserRefundCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.jobisnvillains.UserRefundCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRefundCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRefundCalculatorApplication.class, args);
+	}
+
+}
